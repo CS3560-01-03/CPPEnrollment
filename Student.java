ef7b357fd0607e49ea3a6ccee4e5e6361131e613
aslee28 <81182187+aslee28@users.noreply.github.com>
2021-10-08 19:32:31 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,47 +1,27 @@
 
-public class Student {
+public class Student extends User {
 	public int studentID;
-	public String studentName;
 	public String studentMajor;
-	public String studentYear;
+	public Course courseCompleted;
 	
-	public void createStudent(int studentID) {
-		
+	public Student createStudent(int studentID) {
+		return null;
 	}
+	
 	public void deleteStudent() {
-		
 	}
 	
 	public int getStudentID() {
 		return studentID;
 	}
-	
 	public void setStudentID(int studentID) {
 		this.studentID = studentID;
 	}
-	
-	public String getStudentName() {
-		return studentName;
-	}
-	
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	
 	public String getStudentMajor() {
 		return studentMajor;
 	}
-	
 	public void setStudentMajor(String studentMajor) {
 		this.studentMajor = studentMajor;
-	}
-	
-	public String getStudentYear() {
-		return studentYear;
-	}
-	
-	public void setStudentYear(String studentYear) {
-		this.studentYear = studentYear;
 	}
 
 }
