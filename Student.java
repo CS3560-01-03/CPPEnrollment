3887c50659094378b1daaa3a9f673dcae4a89a0a
aslee28 <81182187+aslee28@users.noreply.github.com>
2021-10-08 18:42:52 -0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,47 @@
+
+public class Student {
+	public int studentID;
+	public String studentName;
+	public String studentMajor;
+	public String studentYear;
+	
+	public void createStudent(int studentID) {
+		
+	}
+	public void deleteStudent() {
+		
+	}
+	
+	public int getStudentID() {
+		return studentID;
+	}
+	
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+	public String getStudentMajor() {
+		return studentMajor;
+	}
+	
+	public void setStudentMajor(String studentMajor) {
+		this.studentMajor = studentMajor;
+	}
+	
+	public String getStudentYear() {
+		return studentYear;
+	}
+	
+	public void setStudentYear(String studentYear) {
+		this.studentYear = studentYear;
+	}
+
+}
