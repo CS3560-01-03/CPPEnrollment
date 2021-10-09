c3cc9fb182dd0c7a7ae6ef008de2dc831619466c
aslee28 <81182187+aslee28@users.noreply.github.com>
2021-10-08 18:30:04 -0700

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,46 @@
+public class Course {
+	public String courseSubject;
+	public String courseCode;
+	public String courseTitle;
+	public String courseDescription;
+	public String coursePrerequisites;
+	public int courseCredit;
+	
+	public String getCourseSubject() {
+		return courseSubject;
+	}
+	public void setCourseSubject(String courseSubject) {
+		this.courseSubject = courseSubject;
+	}
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+	public String getCourseDescription() {
+		return courseDescription;
+	}
+	public void setCourseDescription(String courseDescription) {
+		this.courseDescription = courseDescription;
+	}
+	public String getCoursePrerequisites() {
+		return coursePrerequisites;
+	}
+	public void setCoursePrerequisites(String coursePrerequisites) {
+		this.coursePrerequisites = coursePrerequisites;
+	}
+	public int getCourseCredit() {
+		return courseCredit;
+	}
+	public void setCourseCredit(int courseCredit) {
+		this.courseCredit = courseCredit;
+	}
+	
+}
