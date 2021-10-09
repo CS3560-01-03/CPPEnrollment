172573146915dd42351ac0efd2755fda8dafa5c2
aslee28 <81182187+aslee28@users.noreply.github.com>
2021-10-08 19:26:56 -0700

    Create User.java

---
+++ User.java
@@ -0,0 +1,24 @@
+public class User {
+	public String name;
+	public String address;
+	public String dateOfBirth;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+}
