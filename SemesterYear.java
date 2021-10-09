10faaccec90f1ca01a1f5ccd8c693ecf5046b087
aslee28 <81182187+aslee28@users.noreply.github.com>
2021-10-08 20:46:22 -0700

    Update SemesterYear.java

--- SemesterYear.java
+++ SemesterYear.java
@@ -1,24 +1,8 @@
 public class SemesterYear {
-
-	public int semesterUnits;
-	public int studentID;
+	
+	public Course[] coursesCart;
+	public Course[] enrolledCourses;
 	public String enrollmentDate;
-	
-	public int getSemesterUnits() {
-		return semesterUnits;
-	}
-	
-	public void setSemesterUnits(int semesterUnits) {
-		this.semesterUnits = semesterUnits;
-	}
-	
-	public int getStudentID() {
-		return studentID;
-	}
-	
-	public void setStudentID(int studentID) {
-		this.studentID = studentID;
-	}
 	
 	public String getEnrollmentDate() {
 		return enrollmentDate;
